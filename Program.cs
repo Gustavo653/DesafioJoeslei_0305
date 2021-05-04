58332e59834cc6c6437bb137a2b6358fc178779f
Gustavo Henrique da Silva <ujipe@hotmail.com>
2021-05-04 08:21:44 -0300

    Adicionar arquivos de projeto.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DesafioJoeslei___0305
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
