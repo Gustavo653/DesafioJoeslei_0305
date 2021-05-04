a6dcb4878782cfbbd6eff8339578a027a0b50703
Gustavo Henrique da Silva <ujipe@hotmail.com>
2021-05-04 09:17:49 -0300

    Inimigo Orc concluído

--- Program.cs
+++ Program.cs
@@ -1,10 +1,11 @@
 ﻿using System;
+using System.Threading.Tasks;
 
 namespace DesafioJoeslei___0305
 {
     class Program
     {
-        static void Main(string[] args)
+        static async Task Main(string[] args)
         {
             int diaFloresta = 2;
             int diaEstrada = 1;
@@ -21,6 +22,12 @@ namespace DesafioJoeslei___0305
 
             int vidaGoblin = 10;
             int valorMinimoGob = 2, valorMaximoGob = 5; //Intervalo de dano de Goblin
+
+            int vidaOrc = 12;
+            int valorMinimoOrc = 3, valorMaximoOrc = 7; //Intervalo de dano de Goblin
+
+            int vidaTroll = 20;
+            int valorMinimoTroll = 5, valorMaximoTroll = 9; //Intervalo de dano de Goblin
 
             Console.WriteLine("Joeslei decide ir a capital");
             Console.WriteLine();
@@ -40,6 +47,7 @@ namespace DesafioJoeslei___0305
                         {
                             Console.WriteLine("Seu inimigo é um Goblin");
                             Console.WriteLine("A luta iniciará em breve");
+                            await Task.Delay(2000);
                             while (vidaGoblin > 0 || vidaJoeslei > 0)
                             {
                                 int danoJoeslei = ran.Next(valorMinimo, valorMaximo);
@@ -117,6 +125,81 @@ namespace DesafioJoeslei___0305
                         else if (inimigo <= 9)
                         {
                             Console.WriteLine("Seu inimigo é um Orc");
+                            Console.WriteLine("A luta iniciará em breve");
+                            await Task.Delay(2000);
+                            while (vidaOrc > 0 || vidaJoeslei > 0)
+                            {
+                                int danoJoeslei = ran.Next(valorMinimo, valorMaximo);
+                                int danoOrc = ran.Next(valorMinimoOrc, valorMaximoOrc);
+
+                                Console.WriteLine("Você deseja: 1 - Atacar, 2 - Defender, 3 - Usar item ou 4 - Fugir");
+                                int decisao = int.Parse(Console.ReadLine());
+                                if (decisao == 1)
+                                {
+                                    int danoAtaque = ran.Next(1, 101);
+                                    if (danoAtaque <= 8)
+                                    {
+                                        Console.WriteLine("DANO ORIGINAL: " + danoJoeslei);
+                                        danoJoeslei *= 2;
+                                        Console.WriteLine("DANO DOBRADO: " + danoJoeslei);
+                                    }
+                                    vidaOrc -= danoJoeslei;
+                                    if (danoJoeslei > vidaOrc)
+                                    {
+                                        vidaOrc = 0;
+                                        break;
+                                    }
+                                    vidaJoeslei -= danoOrc;
+                                    if (danoOrc > vidaJoeslei)
+                                    {
+                                        vidaJoeslei = 0;
+                                        break;
+                                    }
+                                    Console.WriteLine("Vida restante do Orc: " + vidaOrc);
+                                    Console.WriteLine("Vida restante do Joeslei: " + vidaJoeslei);
+                                }
+                                else if (decisao == 2)
+                                {
+                                    resistenciaJoeslei *= 2;
+                                    Console.WriteLine("Resistência dobrada: " + resistenciaJoeslei);
+                                    int pularAtaque = ran.Next(1, 11);
+                                    if (pularAtaque <= 4)
+                                    {
+                                        Console.WriteLine("Ataque pulado!");
+                                    }
+                                    else
+                                    {
+                                        vidaJoeslei -= (danoOrc - resistenciaJoeslei);
+                                        if (danoOrc > vidaJoeslei)
+                                        {
+                                            vidaJoeslei = 0;
+                                            break;
+                                        }
+                                        Console.WriteLine("Vida restante do Orc: " + vidaOrc);
+                                        Console.WriteLine("Vida restante do Joeslei: " + vidaJoeslei);
+                                    }
+                                }
+                                else if (decisao == 3)
+                                {
+                                    Console.WriteLine("Função indisponível");
+                                }
+                                else if (decisao == 4)
+                                {
+                                    int fugirAtaque = ran.Next(1, 101);
+                                    if (fugirAtaque <= 35)
+                                    {
+                                        Console.WriteLine("Você conseguiu fugir do ataque!");
+                                        Console.WriteLine("Reiniciando etapa");
+                                        xFloresta--;
+                                    }
+                                }
+                                else
+                                {
+                                    Console.WriteLine("Comando inválido");
+                                }
+                            }
+                            Console.WriteLine("Vida final do Orc: " + vidaOrc);
+                            Console.WriteLine("Vida final do Joeslei: " + vidaJoeslei);
                         }
                         else
                         {
